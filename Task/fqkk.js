13e8140bcd7de151eb495251a39ec411a1bc01c6
zoenzao <80054093+Zoenzao@users.noreply.github.com>
2021-04-01 23:58:14 +0800

    Update fqkk.js

--- Task/fqkk.js
+++ Task/fqkk.js
@@ -67,7 +67,7 @@ let fqkkurlArr = [], fqkkhdArr = [],fqkkbodyArr = [],fqkkcount = ''
 let fqkkurl = $.getdata('fqkkurl')
 let fqkkhd = $.getdata('fqkkhd')
 let fqkey = ''
-let fqkkxh = ($.getval('fqkkxh') || '3');  // 此处修改循环次数，默认一百
+let fqkkxh = ($.getval('fqkkxh') || '10');  // 此处修改循环次数，默认一百
 let fqtx = ($.getval('fqtx') || '200');  // 此处修改提现金额，0.1元等于10，默认为提现一元，也就是100
 let max = 120
 let min = 37
@@ -93,32 +93,6 @@ if ($.isNode()) {
   } else {
    fqkkhdArr = process.env.FQKK_HD.split()
   };
-/*  if (process.env.RLBODY && process.env.RLBODY.indexOf('#') > -1) {
-   rlbody = process.env.RLBODY.split('#');
-   console.log(`您选择的是用"#"隔开\n`)
-  }
-  else if (process.env.RLBODY && process.env.RLBODY.indexOf('\n') > -1) {
-   rlbody = process.env.RLBODY.split('\n');
-   console.log(`您选择的是用换行隔开\n`)
-  } else {
-   rlbody = process.env.RLBODY.split()
-  };*/
-	
- /*  Object.keys(rlurl).forEach((item) => {
-        if (rlurl[item]) {
-          rlurlArr.push(rlurl[item])
-        }
-    });
-    Object.keys(rlheader).forEach((item) => {
-        if (rlheader[item]) {
-          rlheaderArr.push(rlheader[item])
-        }
-    });  	
-    Object.keys(rlbody).forEach((item) => {
-        if (rlbody[item]) {
-          rlbodyArr.push(rlbody[item])
-        }
-    });  */
 	
     console.log(`============ 脚本执行-国际标准时间(UTC)：${new Date().toLocaleString()}  =============\n`)
     console.log(`============ 脚本执行-北京时间(UTC+8)：${new Date(new Date().getTime() + 8 * 60 * 60 * 1000).toLocaleString()}  =============\n`)
